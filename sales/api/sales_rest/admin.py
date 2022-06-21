96a917c262cb81bb06fc21361b35391b3735086c
Spencer <strhyne88@gmail.com>
2022-06-21 14:42:56 -0400

    added modes/views/admin/urls

--- sales/api/sales_rest/admin.py
+++ sales/api/sales_rest/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import SalesPerson, SalesHistory, Customer
+
+@admin.register(SalesPerson)
+class SalesPersonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SalesHistory)
+class SalesHistoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
 # Register your models here.
