4ab45d6090f145620183761e71ed90491fa5245c
Spencer <strhyne88@gmail.com>
2022-06-22 15:42:47 -0400

    fixed my insomnia issues backend should be done now

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class AutomobileVO(models.Model):
     vin = models.CharField(max_length=17, unique=True)
-    import_href = models.CharField(max_length=300, unique=True)
+    # import_href = models.CharField(max_length=300, unique=True)
 
     def __str__(self):
         return self.vin 
--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -5,22 +5,51 @@ import time
 import json
 import requests
 
+
 sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_project.settings")
 django.setup()
 
 # Import models from sales_rest, here.
+from sales_rest.models import AutomobileVO
 # from sales_rest.models import Something
+# def poll():
+#     while True:
+#         print("sales polling for data")
+#         try:
+#             response = requests.get("http://inventory-api:8000/api/automobiles/")
+#             content = json.loads(response.content)
+#             for auto in content["autos"]:                
+#                 AutomobileVO.objects.update_or_create(
+#                     import_href=auto["href"],
+#                     defaults={
+#                         "vin": auto["vin"],
+#                     },
+#                 )
+
+#         except Exception as e:
+#             print(e, file=sys.stderr)
+#         time.sleep(10)
+
+
+# if __name__ == "__main__":
+#     poll()
 
 def poll():
     while True:
         print('Sales poller polling for data')
         try:
+            response = requests.get("http://inventory-api:8000/api/automobiles/")
+            content = json.loads(response.content)
+            for auto in content["autos"]:
+                AutomobileVO.objects.update_or_create(
+                    vin = auto["vin"]
+                )
             # Write your polling logic, here
             pass
         except Exception as e:
             print(e, file=sys.stderr)
-        time.sleep(60)
+        time.sleep(10)
 
 
 if __name__ == "__main__":
